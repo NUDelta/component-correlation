92a6911958dba03ec9c0e764fcdc0c4e77164bb4
Nicole Zhu <nzhu94@gmail.com>
2016-02-16 21:46:21 -0600

    start cleaning css json object

--- processing/script.js
+++ processing/script.js
@@ -63,8 +63,41 @@ function getChildren() {
   var nav_children = $('.page-nav').find('*');
 
   for (var i=0; i< nav_children.length; i++) {
-    console.log($(nav_children[i]).css());
+    var child_css = $(nav_children[i]).css();
+    var child_node = $(nav_children[i])[0].nodeName;
+    var child_class = $(nav_children[i])[0].className;
+    var css = cleanCSS(JSON.stringify(child_css));
+
+    $('#css-output').append('<div class="css-attr">' + child_node.toLowerCase() + ' ' + css + '</div>');
+    // console.log(child_node);
+    // console.log(child_css);
+    // console.log($(nav_children[i]).css());
   }
+}
+
+// need to clean this up but for now i don't know enough regex to do this properly
+function cleanCSS(css) {
+  var cleaned_css = css.replace(/"/g , "");
+  var attr = ['font-family','font-size','font-weight','font-style','color',
+      'text-transform','text-decoration','letter-spacing','word-spacing',
+      'line-height','text-align','vertical-align','direction','background-color',
+      'background-image','background-repeat','background-position',
+      'background-attachment','opacity','width','height','top','right','bottom',
+      'left','margin-top','margin-right','margin-bottom','margin-left',
+      'padding-top','padding-right','padding-bottom','padding-left',
+      'border-top-width','border-right-width','border-bottom-width',
+      'border-left-width','border-top-color','border-right-color',
+      'border-bottom-color','border-left-color','border-top-style',
+      'border-right-style','border-bottom-style','border-left-style','position',
+      'display','visibility','z-index','overflow-x','overflow-y','white-space',
+      'clip','float','clear','cursor','list-style-image','list-style-position',
+      'list-style-type','marker-offset'];
+
+  for (var i = 0; i < attr.length; i++) {
+    var regex = new RegExp('(,' + attr[i] + ')');
+    cleaned_css = cleaned_css.replace(regex, ';' + attr[i]);
+  }
+  return cleaned_css;
 }
 
 $(document).ready(function() {
